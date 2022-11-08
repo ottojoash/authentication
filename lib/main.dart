929f24c8f9aaea52385c06c576d6efdce157ab22
ottojoash <louisjoshbricks@gmail.com>
2022-11-08 01:10:17 -0500

    state widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+      
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+/*create the main layout of the app*/
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
+class LoginScreen extends StateFulWidget
