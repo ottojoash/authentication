54135c6e8edd38f68f394472d9a759778b8651ff
ottojoash <louisjoshbricks@gmail.com>
2022-11-09 03:23:32 -0500

    profile screen

---
+++ lib/profile_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}):super(key: key);
+
+  @override
+  State<_ProfileScreenState> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
