f959c526dc0fa130fe1a6d6149fc69abca6b375b
otto joash <louisjoshbricks@gmail.com>
2022-11-09 01:34:31 -0500

    login function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
 2.login into the firebase
 3.add firebase dependecies for flutter
 4.initate the firebase app
+5. create login functon
 */
 
 class HomePage extends StatefulWidget {
@@ -30,10 +32,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //Initialialze firebase app
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: LoginScreen()
-    );
+    return Scaffold(
+        body: FutureBuilder(
+      future: _initializeFirebase(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return LoginScreen();
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    ));
   }
 }
 
@@ -45,6 +63,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  //login function
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -52,57 +72,69 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children:  [
-          const Text("My APP TITLE",style: TextStyle(color: Colors.black,
-           fontSize:28.0,
-           fontWeight: FontWeight.bold),
-           ),
-           const Text("Login to your app",
-           style: TextStyle(color: Colors.black,
-           fontSize: 44.0,
-           fontWeight: FontWeight.bold),
-           ),//text
-           const SizedBox(
+        children: [
+          const Text(
+            "My APP TITLE",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold),
+          ),
+          const Text(
+            "Login to your app",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold),
+          ), //text
+          const SizedBox(
             height: 44.0,
+          ),
+          const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "user email",
+              prefixIcon: Icon(Icons.mail, color: Colors.black),
             ),
-            const TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: "user email",
-                prefixIcon: Icon(Icons.mail,color: Colors.black),
-              ),
+          ),
+          const SizedBox(
+            height: 26.0,
+          ),
+          const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "user password",
+              prefixIcon: Icon(Icons.lock, color: Colors.black),
             ),
-            const SizedBox(
-              height: 26.0,
-            ),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "user password",
-                prefixIcon: Icon(Icons.lock, color: Colors.black),
-              ),
-            ),
-            const SizedBox(height: 12.0,
-            ),
-            const Text("Dont remember your password?",
+          ),
+          const SizedBox(
+            height: 12.0,
+          ),
+          const Text(
+            "Dont remember your password?",
             style: TextStyle(color: Colors.blue),
-            ),
-            const SizedBox(
-              height: 88.0,
-            ),
-            Container(
-              width: double.infinity,
-              child: RawMaterialButton(
-                fillColor: Color(0xFF0069FE),
-                elevation: 0.0,
-                padding: EdgeInsets.symmetric(vertical: 20.0),
-                shape: RoundedRectangleBorder(
+          ),
+          const SizedBox(
+            height: 88.0,
+          ),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12.0)),
-                onPressed: (){},
-                child:Text("Login",style:TextStyle(
+              onPressed: () {},
+              child: Text(
+                "Login",
+                style: TextStyle(
                   color: Colors.white,
-                  fontSize: 18.0,),),),
+                  fontSize: 18.0,
+                ),
+              ),
             ),
+          ),
         ],
       ),
     );
